b259de90c60224726b0dc16854f43bf18e9a810f
MyHorizonn <infernalfonik@gmail.com>
2020-09-27 16:48:25 +0400

    lab2

--- lab2.py
+++ lab2.py
@@ -1,10 +1,16 @@
+import matplotlib.pyplot as plt
+import math
+import numpy as np
+from random import randint, seed
+
+
 def main():
     print("A")
-    A = float(input())
+    a = float(input())
     print("B")
-    B = float(input())
+    b = float(input())
     print("C")
-    C = float(input())
+    c = float(input())
     print("alpha")
     alpha = float(input())
     print("beta")
@@ -13,13 +19,46 @@ def main():
     gamma = float(input())
     print("signal")
     kek = input()
-    signal_x = [x for x in kek]
+    signal_x = [i for i in kek]
     print(signal_x)
+    x = list()
+    temp = 0.8
+    for i in range(len(signal_x)):
+        temp += 0.2
+        x.append(temp)
+    print(x)
+    y = list()
+    for i in range(len(x)):
+        y.append(a * math.sin(alpha * x[i]) + b * math.cos(beta * x[i]) + x[i] * c * math.cos(math.cos(gamma * x[i])))
+    print(y)
     print("key")
-    k = int(input())
-    signal_y = signal_x
-    print(signal_y)
+    key = int(input())
+    y1 = list()
+    seed(key)
+    for i in range(len(x)):
+        y1.append(randint(0, len(x)))
+    print(y1)
+    print(signal_x)
+    plt.subplot(2, 2, 1)
+    plt.plot(x, y, 'o-g', fillstyle='none', ls='-', lw=2.5)
+    plt.grid(True)
+    for i in range(len(y)):
+        temp = y[i]
+        y[i] = y[y1[i]]
+        y[y1[i]] = temp
+        print(i, y1[i])
+    plt.subplot(2, 2, 2)
+    plt.plot(x, y, 'o-g', fillstyle='none', ls='-', lw=2.5)
+    plt.grid(True)
+    for i in reversed(range(len(y))):
+        temp = y[i]
+        y[i] = y[y1[i]]
+        y[y1[i]] = temp
+        print(i, y1[i])
+    plt.subplot(2, 2, 3)
+    plt.plot(x, y, 'o-g', fillstyle='none', ls='-', lw=2.5)
+    plt.grid(True)
+    plt.show()
 
 
-print(list(x for x in "abx"))
 main()
